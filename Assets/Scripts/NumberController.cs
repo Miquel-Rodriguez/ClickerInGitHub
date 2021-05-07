d60baaa26a3f4876961e6703c1208d831b061b0b
Miquel <miquel.rodriguez.7e3@itb.cat>
2021-05-07 16:15:50 +0200

    que si queso

---
+++ Assets/Scripts/NumberController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberController : MonoBehaviour
+{
+
+    [SerializeField] EnergyBar energyBarController;
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+   
+}
