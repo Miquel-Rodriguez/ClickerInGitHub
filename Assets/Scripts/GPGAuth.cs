b1c6bd42e68d198263cf29ecb7b9167da0100f31
Adrian <adrian.nunyez.7e3@itb.catr>
2021-05-10 14:12:48 +0200

    .

---
+++ Assets/Scripts/GPGAuth.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine.SocialPlatforms;
+
+public class GPGAuth : MonoBehaviour
+{
+    public static PlayGamesPlatform platform;
+    
+    void Start()
+    {
+        if (platform == null)
+        {
+            PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
+            PlayGamesPlatform.InitializeInstance(config);
+            PlayGamesPlatform.DebugLogEnabled = true;
+
+            platform = PlayGamesPlatform.Activate();
+        }
+
+        Social.Active.localUser.Authenticate(success =>
+        {
+            if (success)
+            {
+                Debug.Log("Loged in");
+            }
+            else
+            {
+                Debug.Log("Failed to login");
+            }
+        });
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
