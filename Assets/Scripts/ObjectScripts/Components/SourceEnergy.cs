e4de685ce284b79c5b5aa190421d2b08cdb22f15
Miquel <miquel.rodriguez.7e3@itb.cat>
2021-05-12 10:21:35 +0200

    Finalizacíon de clases de los componentes y Info Panel
    al menos de momento la cosa está acabada

--- Assets/Scripts/ObjectScripts/Components/SourceEnergy.cs
+++ Assets/Scripts/ObjectScripts/Components/SourceEnergy.cs
@@ -6,13 +6,11 @@ public class SourceEnergy : Component
 {
     public float maxEnergy;
     public float energyForSecond;
-    public float energyCostForClick;
 
     private void Start()
     {
-        statsDescription = "Max Energy Capacity: "+maxEnergy+"\n" +
-            "Energy recover: "+energyForSecond+"\n"+
-            "Cost per Click: "+energyCostForClick;
+        statsDescription = "Max Energy Capacity: " + maxEnergy + "\n" +
+            "Energy recover: " + energyForSecond;
     }
 
 
---
+++ Assets/Scripts/ObjectScripts/Components/Storage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Storage : Component
+{
+    public float maxBitesCapacity;
+
+    private void Start()
+    {
+        statsDescription = "Max bites capacity: " + maxBitesCapacity;
+    }
+}
