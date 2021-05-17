10750ae94c70bab4eb6b56b84cd5d5c2f236ea1a
EvilJMS <josep.mora.7e3@itb.cat>
2021-05-17 19:02:42 +0200

    recyler view missions

---
+++ Assets/Scripts/RecyclersViews/MissionRecyclerView.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MissionRecyclerView : UI.RecyclerView<MissionRecyclerView.Holder>.Adapter
+{
+    public List<Mission> missions;
+    // Start is called before the first frame update
+    void Start()
+    {
+        NotifyDatasetChanged();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+
+    public override int GetItemCount()
+    {
+        return missions.Count;
+    }
+
+    public override void OnBindViewHolder(Holder holder, int i)
+    {
+        holder.nameText.text = missions[i].missionName;
+        holder.descText.text = missions[i].missionDescription;
+        holder.bitsText.text = BitUtil.StringFormat(missions[i].requiredBits, BitUtil.TextFormat.Long);
+    }
+
+    
+    public override GameObject OnCreateViewHolder()
+    {
+        return Instantiate(row);
+    }
+
+    public class Holder : ViewHolder
+    {
+        public TextMeshProUGUI nameText;
+        public TextMeshProUGUI descText;
+        public TextMeshProUGUI bitsText;
+        public TextMeshProUGUI rewardText;
+        public Holder(GameObject itemView) : base(itemView)
+        {
+            nameText = itemView.transform.Find("Name").GetComponent<TextMeshProUGUI>();
+            descText = itemView.transform.Find("Description").GetComponent<TextMeshProUGUI>();
+            bitsText = itemView.transform.Find("BitsRequired").GetComponent<TextMeshProUGUI>();
+        }
+    }
+}
