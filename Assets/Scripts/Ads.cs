d3c51149bee71147efe28a8ac51975cfe660b33d
Adrian <adrian.nunyez.7e3@itb.catr>
2021-05-19 14:06:18 +0200

    First banner

---
+++ Assets/Scripts/Ads.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class Ads : MonoBehaviour
+{
+    private BannerView bannerView;
+    private string baner = "ca-app-pub-3940256099942544/6300978111";
+    public void Start()
+    {
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.Initialize(initStatus => { });
+
+        this.RequestBanner();
+    }
+
+    private void RequestBanner()
+    {
+
+        this.bannerView = new BannerView(baner, AdSize.Banner, AdPosition.Bottom);
+        AdRequest request = new AdRequest.Builder().Build();
+        this.bannerView.LoadAd(request);
+    }
+}
