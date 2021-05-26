f283a9d7138fa749701185578a429c49ead91b74
Adrian <adrian.nunyez.7e3@itb.catr>
2021-05-26 21:04:23 +0200

    Creo que todo está bien

--- Assets/Scripts/GetUsername.cs
+++ Assets/Scripts/GetUsername.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using GooglePlayGames;
 
 public class GetUsername : MonoBehaviour
 {
-    [SerializeField] TextMesh usernameText;
+    [SerializeField] TextMeshProUGUI usernameText;
     void Start()
     {
        usernameText.text = Social.localUser.userName.ToString();       
