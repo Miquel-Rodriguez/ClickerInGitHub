1e18d97b897a6ea4bd39378b467d35ef66c8e878
Miquel <miquel.rodriguez.7e3@itb.cat>
2021-05-28 11:50:24 +0200

    j

--- Assets/Scripts/TutorialController.cs
+++ Assets/Scripts/TutorialController.cs
@@ -26,99 +26,150 @@ public class TutorialController : MonoBehaviour
     [SerializeField] GameObject T6;
     [SerializeField] GameObject T7;
     [SerializeField] GameObject T8;
+    [SerializeField] GameObject All;
+
+    bool activeTutorial=true;
 
     void Awake()
     {
-        PlayerPrefs.DeleteAll();
+       
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        T1.SetActive(true);
+
+        if (PlayerPrefs.GetInt("activeTutorial", 1) == 1)
+        {
+            activeTutorial = true;
+        }
+        else activeTutorial = false;
+
+       
+        if (activeTutorial)
+        {
+            T1.SetActive(true);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (firstStop.activeInHierarchy && Input.GetMouseButtonDown(0))
-        {
-            firstStop.SetActive(false);
-        }
-
-        if (bits.currentBits >= 12)
-        {
-            if (aux == false)
+        if (activeTutorial) {
+            if (firstStop.activeInHierarchy && Input.GetMouseButtonDown(0))
             {
-                T1.SetActive(false);
-                secondStop.SetActive(true);
-                secondStop2.SetActive(true);
-                secondStop3.SetActive(true);
-                aux = true;
+                firstStop.SetActive(false);
             }
-           
+
+            if (bits.currentBits >= 12)
+            {
+                if (aux == false)
+                {
+                    T1.SetActive(false);
+                    secondStop.SetActive(true);
+                    secondStop2.SetActive(true);
+                    secondStop3.SetActive(true);
+                    aux = true;
+                }
+
+            }
+
+            if (secondStop.activeInHierarchy && PcPanel.activeInHierarchy)
+            {
+                secondStop.SetActive(false);
+                secondStop2.SetActive(false);
+                secondStop3.SetActive(false);
+                T2.SetActive(true);
+            }
+
+            if (shop.activeInHierarchy && shopStop.activeInHierarchy)
+            {
+                shopStop.SetActive(false);
+                shopStop2.SetActive(false);
+                shopStop3.SetActive(false);
+                shopStop4.SetActive(false);
+            }
+
         }
 
-        if (secondStop.activeInHierarchy && PcPanel.activeInHierarchy)
-        {
-            secondStop.SetActive(false);
-            secondStop2.SetActive(false);
-            secondStop3.SetActive(false);
-            T2.SetActive(true);
-        }
-
-        if (shop.activeInHierarchy && shopStop.activeInHierarchy)
-        {
-            shopStop.SetActive(false);
-            shopStop2.SetActive(false);
-            shopStop3.SetActive(false);
-            shopStop4.SetActive(false);
-        }
     }
 
 
     public void ActiveInformationPanelTutrial()
     {
-        T2.SetActive(false);
-        T3.SetActive(true);
+        if (activeTutorial) {
+            T2.SetActive(false);
+            T3.SetActive(true);
+        }
+
     }
 
     public void ActivePanelGoToGaccha()
     {
-        T3.SetActive(false);
-        T4.SetActive(true);
+        if (activeTutorial)
+        {
+            T3.SetActive(false);
+            T4.SetActive(true);
+        }
+
     }
 
     public void ActivePanelGaccha()
     {
-        T4.SetActive(false);
-        T5.SetActive(true);
+        if (activeTutorial)
+        {
+            T4.SetActive(false);
+            T5.SetActive(true);
+        }
+
     }
 
     public void DesactiveGacha()
     {
-        T5.SetActive(false);
+        if (activeTutorial)
+        {
+            T5.SetActive(false);
+        }
+
     }
 
     public void ActiveReturnToMainScreen()
     {
-        T6.SetActive(true);
+        if (activeTutorial)
+        {
+            T6.SetActive(true);
+        }
+
     }
 
     public void ActiveT7()
     {
-        T6.SetActive(false);
-        T7.SetActive(true);
+        if (activeTutorial)
+        {
+            T6.SetActive(false);
+            T7.SetActive(true);
+        }
+
     }
 
     public void ActiveT8()
     {
-        T7.SetActive(false);
-        T8.SetActive(true);
+        if (activeTutorial)
+        {
+            T7.SetActive(false);
+            T8.SetActive(true);
+        }
+
     }
 
     public void closeAll()
     {
-
+        if (activeTutorial)
+        {
+            All.SetActive(false);
+            T8.SetActive(false);
+            activeTutorial = false;
+            PlayerPrefs.SetInt("activeTutorial",0);
+        }
     }
 }
