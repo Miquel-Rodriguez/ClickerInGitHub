0deee9ef5d8695b4168d578153392338e7f188b5
Adrian <adrian.nunyez.7e3@itb.catr>
2021-05-28 13:22:32 +0200

    Poniendo logros

--- Assets/Scripts/GPHAchievements.cs
+++ Assets/Scripts/GPHAchievements.cs
@@ -5,19 +5,50 @@ using GooglePlayGames;
 
 public class GPHAchievements : MonoBehaviour
 {
+    [SerializeField] NumberController bits;
     public void openAchievementPanel()
     {
         Social.ShowAchievementsUI();
     }
 
-    public void UpdateIncremental()
+    public void Tap10Times()
     {
         PlayGamesPlatform.Instance.IncrementAchievement(GPGSIds.achievement_tapping_begginer, 1, null);
     }
 
-    public void UnlockRegular()
+    public void UnlockShop()
     {
         Social.ReportProgress(GPGSIds.achievement_shopping, 100f, null);
+    }
+
+    public void UnlockCredits()
+    {
+        Social.ReportProgress(GPGSIds.achievement_a_pretty_short_credits, 100f, null);
+    }
+
+    public void Get10000Bits()
+    {
+        PlayGamesPlatform.Instance.IncrementAchievement(GPGSIds.achievement_getting_bits, (int)bits.currentBits, null);
+    }
+
+    public void Get100000Bits()
+    {
+        Social.ReportProgress(GPGSIds.achievement_more_and_more_bits, 100f, null);
+    }
+
+    public void GetAMillionBits()
+    {
+        Social.ReportProgress(GPGSIds.achievement_a_million, 100f, null);
+    }
+
+    public void GetABillionBits()
+    {
+        Social.ReportProgress(GPGSIds.achievement_a_billion, 100f, null);
+    }
+
+    public void GetAQuadrillionBits()
+    {
+        Social.ReportProgress(GPGSIds.achievement_more_bits_than_elon_musk, 100f, null);
     }
 
 }
